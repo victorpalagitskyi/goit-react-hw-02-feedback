ed55e7268b80911d634fa46d819230989293089b
Victor20012029103 <112655698+Victor20012029103@users.noreply.github.com>
2023-04-05 12:54:25 +0300

    3

--- src/components/Counter/counter.jsx
+++ src/components/Counter/counter.jsx
@@ -1,6 +1,5 @@
 import { Component } from 'react'
-let total = 0
-let percent = 0
+
 class Counter extends Component {
     
     state = {
@@ -8,52 +7,28 @@ class Counter extends Component {
         neutral: 0,
         bad: 0
     }
-    handleClickIncrementGood = () =>  
+    handleClickIncrement = (type) =>
         this.setState((prevState) => ({
-            good: prevState.good + 1,
-        }),
-            console.log(this.state.good),
-            this.countTotalFeedback(),
-            this.countPositiveFeedbackPercentage()
+            [type]: prevState[type] + 1,
+        }), console.log(type)
         ) 
-        
+   
     
-
-    handleClickIncrementNetural = () =>
-        this.setState((prevState) => ({
-            neutral: prevState.neutral + 1,
-        }),
-            this.countTotalFeedback(),
-            this.countPositiveFeedbackPercentage()
-        )
-    handleClickIncrementBad = () =>
-        this.setState((prevState) => ({
-            bad: prevState.bad + 1,
-        }),
-            this.countTotalFeedback(),
-            this.countPositiveFeedbackPercentage()
-        )
-    countTotalFeedback () {
-      total = this.state.bad + this.state.good + this.state.neutral +1
-    }
-    countPositiveFeedbackPercentage() { 
-     percent = this.state.good/total*100
-    }
     render() {  
         return (
             <div>
                 <div style={{ display: 'flex', gap: "10px", fontSize: 50  }}>
-            <button style={{ fontSize: 50}} onClick={this.handleClickIncrementGood}>Good</button>
-            <button style={{ fontSize: 50}} onClick={this.handleClickIncrementNetural}>Natural</button>
-            <button style={{ fontSize: 50}} onClick={this.handleClickIncrementBad}>Bad</button>
+            <button style={{ fontSize: 50}} onClick={this.handleClickIncrement}>Good</button>
+            <button style={{ fontSize: 50}} onClick={this.handleClickIncrement}>Natural</button>
+            <button style={{ fontSize: 50}} onClick={this.handleClickIncrement}>Bad</button>
             
                 </div>
                 <h2>Statistics</h2>
                 <p>Good:{this.state.good}</p>
                 <p>Netural:{this.state.neutral}</p>
                 <p>Bad:{this.state.bad}</p>
-                <p>Total: {total}</p>
-                <p>Positive feedback: {percent}%</p>
+                <p>Total: {this.state.bad + this.state.good + this.state.neutral }</p>
+                <p>Positive feedback: {this.state.good/(this.state.bad + this.state.good + this.state.neutral )*100}%</p>
             </div>
         )
     }  
