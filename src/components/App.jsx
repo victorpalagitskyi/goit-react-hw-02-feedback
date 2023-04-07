da8768c65880fb2c1ff2d8fe80935ab81b3f288c
Victor20012029103 <112655698+Victor20012029103@users.noreply.github.com>
2023-04-07 23:49:38 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,54 @@
 
-import Counter from "./Counter/counter";
+import { Component } from "react";
+import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
+import { Section } from "./Section/Section";
+import { Statistics } from "./Statistics/statistics";
+import {Notification } from "./Notification/Notification"
 
-export const App  = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <h1> Please leave feedback</h1>
-      <Counter /> 
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+  handleClickIncrement = (type) =>
+    this.setState((prevState) => ({
+      [type]: prevState[type] + 1,
+    }))
+  countTotalFeedback() {
+    return this.state.bad + this.state.good + this.state.neutral
+  }
+  countPositiveFeedbackPercentage() {
+    return (this.state.good / this.countTotalFeedback()) * 100
+  }
+  
+  render() {
+    // console.log(this.state)
+    const { good, bad, neutral } = this.state
+    return (
+      <div
+        style={{
+          height: '100vh',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        <Section title="Please Leave Feedback" >
+          <FeedbackOptions handleClickIncrement={this.handleClickIncrement} />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() === 0 ? (<Notification message="There is no feedback" />) : (
+            <Statistics
+              bad={this.bad}
+              good={this.good}
+              neutral={this.neutral}
+              total={this.countTotalFeedback()}
+              percent={this.countPositiveFeedbackPercentage()}
+            />)}
+        </Section>
+      </div>
+    );
+  }
+}
 
+export default App
